081e76bd7bdfd19de4eebbee74a53f67823ceb7a
mackerelstorm <geiri12345@gmail.com>
2021-07-14 01:00:04 +0200

    Original source files

---
+++ Snitt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+int Antall;
+int Karakter;
+int KarakterTotal = 0;
+int InputAntall = 0;
+printf("Hvor mange karakterer? ");
+scanf("%d", &Antall);
+while (Antall > InputAntall) {
+    printf("Skriv inn en karakter: ");
+    scanf("%d", &Karakter);
+    if (Karakter > 6) {
+        printf("Ugyldig karakter");
+    }
+    else {
+    KarakterTotal = KarakterTotal + Karakter;
+    InputAntall++;   
+    }
+}
+printf("Ditt snitt er: %.2f",(double)KarakterTotal / (double)Antall);
+while (1>0) {
+
+}
+}
---
+++ abc.py
@@ -0,0 +1,22 @@
+import math
+a = input("A: ")
+b = input("B: ")
+c = input("C: ")
+b_minus = int(b) - (2*int(b))
+plus_result = (b_minus + math.sqrt(pow(int(b), 2) - (4 * int(a) * int(c)))) / (2 * int(a))
+minus_result = (b_minus - math.sqrt(pow(int(b), 2) - (4 * int(a) * int(c)))) / (2 * int(a))
+
+if plus_result >=0:
+    plus_factorconvert = "(x-{})".format(int(plus_result))
+else:
+    plus_print = plus_result - 2*plus_result
+    plus_factorconvert = "(x+{})".format(int(plus_print))
+if minus_result >=0:
+    minus_factorconvert = "(x-{})".format(int(minus_result))
+else:
+    minus_print = minus_result - 2*minus_result
+    minus_factorconvert = "(x+{})".format(int(minus_print))
+
+
+print (int(plus_result), (plus_factorconvert))
+print (int(minus_result), (minus_factorconvert))
