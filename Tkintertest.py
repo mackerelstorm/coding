9e4ba6c1c021194329c71182b1dca1e635bcb278
mackerelstorm <geiri12345@gmail.com>
2021-07-16 21:57:50 +0200

    Creating random number generator in Tkinter

--- Tkintertest.py
+++ Tkintertest.py
@@ -1,6 +1,21 @@
 from tkinter import *
+import random
 
+# Configure tkinter window
 window = Tk()
 window.title("My app window")
 window.configure(background="white")
 
+# Defining num for random number generation between 1-100
+num = random.randint(1,100)
+
+# Defining function buttonClick
+def buttonClick():
+    myLabel = Label(window, text=num)
+    myLabel.pack()
+
+# Creating button for generating Random number
+PhysButton = Button(window, text="Generate random number", command=buttonClick)
+PhysButton.pack()
+
+window.mainloop()
