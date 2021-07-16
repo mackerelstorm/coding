68b8ad8bafed116d7d2bcbf659537cd3df09bec1
mackerelstorm <geiri12345@gmail.com>
2021-07-16 22:07:08 +0200

    Changed name and made the window bigger

--- Tkintertest.py
+++ Tkintertest.py
@@ -3,7 +3,7 @@ import random
 
 # Configure tkinter window
 window = Tk()
-window.title("My app window")
+window.title("Random number generator")
 window.configure(background="white")
 
 # Defining num for random number generation between 1-100
@@ -11,11 +11,11 @@ num = random.randint(1,100)
 
 # Defining function buttonClick
 def buttonClick():
-    myLabel = Label(window, text=num)
+    myLabel = Label(window, text=num, width=50, height=5, font=("Arial", 12))
     myLabel.pack()
 
 # Creating button for generating Random number
-PhysButton = Button(window, text="Generate random number", command=buttonClick)
+PhysButton = Button(window, text="Generate random number", command=buttonClick, width=50, height=10, font=("Arial", 12))
 PhysButton.pack()
 
 window.mainloop()
